d1e1479802eb6855c5a824346e5d5bdb953fc6e3
developerthierry <developerthierry@gmail.com>
2025-12-23 00:18:35 +0000

    Layout & Header

---
+++ components/NavItems.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { NAV_ITEMS } from '@/lib/constants'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const NavItems = () => {
+    const pathname = usePathname()
+    const isActive = (path:string) => {
+        if (path === '/') return pathname === '/'
+        return pathname.startsWith(path)
+    }
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+       {NAV_ITEMS.map(({href, label}) => <li key={href} className={`hover:text-yellow-500 transition-colors ${isActive(href) ? 'text-gray-100' : '' }`}>{label}</li>)}
+    </ul>
+  )
+}
+
+export default NavItems
