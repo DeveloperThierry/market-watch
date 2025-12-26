3a4202beb0e7702b325323149214fdace6803422
developerthierry <developerthierry@gmail.com>
2025-12-26 18:26:32 +0000

    Form Fields

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,41 +1,89 @@
-'use client'
+"use client";
 import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
+import { INVESTMENT_GOALS } from "@/lib/constants";
 import { useForm, SubmitHandler } from "react-hook-form";
 const SignUp = () => {
-    const { register, handleSubmit, control, formState: { errors, isSubmitting } } = useForm<SignUpFormData>({
-        defaultValues:{
-
-            fullName:'',
-            email:'',
-            password:'',
-            country:'US',
-            investmentGoals:'Growth',
-            riskTolerance:'Medium',
-            preferredIndustry:'Technology',
-        },
-        mode:'onBlur'
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting },
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology",
+    },
+    mode: "onBlur",
+  });
+  const onSubmit: SubmitHandler<SignUpFormData> = async (
+    data: SignUpFormData
+  ) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error();
     }
-    );
-    const onSubmit: SubmitHandler<SignUpFormData> = async (data:SignUpFormData) => {
-        try{console.log(data)}
-        catch(error){console.error()}
-    };
-  
+  };
+
   return (
     <>
       <h1 className="form-title">Sign Up & Personalize</h1>
       <form action="" onSubmit={handleSubmit(onSubmit)} className="space-y-5">
         <InputField
-            name="fullname" 
-            label="Full Name" 
-            placeholder="John Doe" 
-            register={register} 
-            error={errors.fullName}
-            validation={{required:'Full name is required', minLength:2}}
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLength: 2 }}
         />
-        <Button type="submit" disabled={isSubmitting} className="yellow-btn w-full mt-5">
-            {isSubmitting ? 'Creating Account': 'Start Your Investment Journey'}
+        <InputField
+          name="email"
+          label="Email"
+          placeholder="contact@gmail.com"
+          register={register}
+          error={errors.email}
+          validation={{
+            required: "Email address is required",
+            pattern: /^\w+@\w+\.\w+$/,
+            message: "Email address is required",
+          }}
+        />
+        <InputField
+          name="password"
+          label="Password"
+          type="password"
+          placeholder="Enter a strong password"
+          register={register}
+          error={errors.password}
+          validation={{
+            required: "Email address is required",
+            minLength:0
+          }}
+        />
+        {/* Country */}
+        <SelectField
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goal"
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+        <Button
+          type="submit"
+          disabled={isSubmitting}
+          className="yellow-btn w-full mt-5"
+        >
+          {isSubmitting ? "Creating Account" : "Start Your Investment Journey"}
         </Button>
       </form>
     </>
